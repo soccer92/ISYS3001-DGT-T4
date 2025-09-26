24a2bca5a72d5d258b42e990021405009b3ac3c2
Tom Carpenter <carpth@MacBookPro.lan>
2025-09-26 15:14:37 +1000

    Tidied up spacing and comments.

--- src/db/db.js
+++ src/db/db.js
@@ -6,18 +6,18 @@ Exposes getDB() for queries
 Provides CLI: --init and --seed
 */
 
-import 'dotenv/config'; //loads enviroment variables from .env file
+import 'dotenv/config'; // Loads enviroment variables from .env file
 import Database from 'better-sqlite3'; 
 import fs from 'fs'
 import path from 'path';
 import {fileURLToPath} from 'url';
 
-//finds where this file is located in directory
+// Finds where this file is located in directory.
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-//Resolve the DB path:
-//if DB_PATH is absolute, use it as is
-//if DB_PATH is relative, resolve from project root
+// Resolve the DB path:
+// If DB_PATH is absolute, use it as is.
+// If DB_PATH is relative, resolve from project root.
 const DB_PATH = process.env.DB_PATH
   ? (path.isAbsolute(process.env.DB_PATH)
       ? process.env.DB_PATH
@@ -25,16 +25,16 @@ const DB_PATH = process.env.DB_PATH
   : path.join(process.cwd(), '.localdb', 'tasks.sqlite');
 
 export function getDb() {
-  //ensures the DB directory exists
+  // Ensures the DB directory exists.
   fs.mkdirSync(path.dirname(DB_PATH), {recursive: true});
-  //open the DB
+  // Open the DB.
   const db = new Database(DB_PATH);
   db.pragma('journal_mode = WAL');
   return db;
 }
 
-// CLI helpers
-// create usage for npm run db:init/seed, to create table via schema.sql and insert sample rows via seed.sql
+// CLI helpers.
+// Create usage for npm run db:init/seed, to create table via schema.sql and insert sample rows via seed.sql.
 
 if (process.argv.includes('--init')) {
   const db = getDb();
@@ -49,5 +49,5 @@ if (process.argv.includes('--seed')) {
   const seedPath = path.join(__dirname, 'seed.sql');
   const seedSql = fs.readFileSync(seedPath, 'utf8');
   db.exec(seedSql);
-  console.log('DB seeded with sample data');
+  console.log('DB seeded with sample data.');
 }
